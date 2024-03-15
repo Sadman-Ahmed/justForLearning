939c3e8594b229c0e2a60bc44a55c1cbd5641c10
Sadman-Ahmed <sdmn.ahmd@gmail.com>
2024-03-15 12:18:31 +0600

    added division function

--- Main.c
+++ Main.c
@@ -3,6 +3,7 @@
 int add(int num1, int num2);
 int sub(int a, int b);
 int mul(int a, int b);
+int div(int a, int b);
 
 int main(){
     int num1, num2;
@@ -17,10 +18,12 @@ int main(){
     int addRes = add(num1, num2);
     int subRes = sub(num1,num2);
     int mulRes = mul(num1, num2);
+    int divRes = div(num1, num2);
 
-    printf("The sum of the number is: %d\n",addRes);
-    printf("The substraction of the number is: %d\n",subRes);
+    printf("The sum of the numbers are: %d\n",addRes);
+    printf("The substraction of the numbers are: %d\n",subRes);
     printf("The multiplication of the numbers are: %d",mulRes);
+    printf("The division of the numbers are: %d",divRes);
 
     return 0;
 
@@ -31,9 +34,13 @@ int add(int a, int b){
 }
 
 int sub(int a, int b){
-    return a - b;
+    return a-b;
 }
 
 int mul(int a, int b){
     return a*b;
+}
+
+int div(int a, int b){
+    return a/b;
 }
