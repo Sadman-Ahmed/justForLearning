0ec753b090b8c36a7665527711559a83bc6e0a20
RidowanShadid <161294459+RidowanShadid@users.noreply.github.com>
2024-03-15 16:41:47 +1100

    substraction function added

--- Main.c
+++ Main.c
@@ -7,8 +7,10 @@ int main(){
     int num2 = 20;
 
     int result = add(num1, num2);
+    int result1 = minus(num1,num2);
 
     printf("The sum of the number is: %d", result);
+    printf("The substraction of the number is: %d",result1);
 
     return 0;
 
@@ -16,4 +18,7 @@ int main(){
 
 int add(int a, int b){
     return a+b;
+}
+int minus(int a, int b){
+    return a - b;
 }
