eb7725c2bfb9c69c19e54a7e45f73c258bfda9b6
Sadman-Ahmed <sdmn.ahmd@gmail.com>
2024-03-15 12:13:59 +0600

    added user input function

--- Main.c
+++ Main.c
@@ -2,18 +2,25 @@
 
 int add(int num1, int num2);
 int sub(int a, int b);
-int mal(int a, int b);
+int mul(int a, int b);
 
 int main(){
-    int num1 = 10;
-    int num2 = 20;
+    int num1, num2;
+
+    
+    printf("enter a number: ");
+    scanf("%d", &num1);
+    
+    printf("enter the smaller number than the previous number: ");
+    scanf("%d", &num2);
 
     int addRes = add(num1, num2);
-    int subRes = sub(num2,num1);
-    int malres = mal(num1, num2);
+    int subRes = sub(num1,num2);
+    int mulRes = mul(num1, num2);
 
     printf("The sum of the number is: %d\n",addRes);
-    printf("The substraction of the number is: %d",subRes);
+    printf("The substraction of the number is: %d\n",subRes);
+    printf("The multiplication of the numbers are: %d",mulRes);
 
     return 0;
 
@@ -22,9 +29,11 @@ int main(){
 int add(int a, int b){
     return a+b;
 }
+
 int sub(int a, int b){
     return a - b;
 }
-int mal(int a, int b){
+
+int mul(int a, int b){
     return a*b;
 }
